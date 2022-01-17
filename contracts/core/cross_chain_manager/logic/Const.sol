a8c00c11fafd3244829a35c5fb4e724a97544f40
KSlashh <48985735+KSlashh@users.noreply.github.com>
2022-01-17 15:42:30 +0800

    update config

--- contracts/core/cross_chain_manager/logic/Const.sol
+++ contracts/core/cross_chain_manager/logic/Const.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.5.0;
 contract Const {
     bytes constant ZionCrossChainManagerAddress = hex"5747C05FF236F8d18BB21Bc02ecc389deF853cae"; 
     
-    address constant EthCrossChainDataAddress = 0x7614A4819cd5226886E06647287E3ADf0CC14756; 
-    address constant EthCrossChainCallerFactoryAddress = 0x13ea6bF39fa8241646ca7080445c40e4AA4F24df; 
-    uint constant chainId = 1000006; 
+    address constant EthCrossChainDataAddress = 0x307E808a65876178aCb919a6e7A1f5a33369BbDf; 
+    address constant EthCrossChainCallerFactoryAddress = 0x7f09148147E03C24Ad0C1df59d73F50CBaeA517e; 
+    uint constant chainId = 107; 
 }
