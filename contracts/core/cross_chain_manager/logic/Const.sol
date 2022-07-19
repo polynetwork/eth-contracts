f2ee4e8b4f3d1053d54489caa369b3c1a59dd60e
KSlashh <48985735+KSlashh@users.noreply.github.com>
2022-07-19 11:41:01 +0800

    merge

--- contracts/core/cross_chain_manager/logic/Const.sol
+++ contracts/core/cross_chain_manager/logic/Const.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.5.0;
 contract Const {
-    bytes constant ZionCrossChainManagerAddress = hex"5747C05FF236F8d18BB21Bc02ecc389deF853cae"; 
+    bytes constant ZionCrossChainManagerAddress = hex"0000000000000000000000000000000000001003"; 
     
     address constant EthCrossChainDataAddress = 0xd2bfdbA1aB74f287c856Df3A550ff6067A302a78; 
     address constant EthCrossChainCallerFactoryAddress = 0xDED94FEAfAaca7064CE45Ff875695f5ad3DEC3E8; 
