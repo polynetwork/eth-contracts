c2391b9b0e5d6741f3722f4509fe2ab3c6e835e7
KSlashh <48985735+KSlashh@users.noreply.github.com>
2022-08-01 10:52:02 +0800

    hexToCompact: empty hex

--- contracts/core/cross_chain_manager/libs/ECCUtils/EthCrossChainUtils.sol
+++ contracts/core/cross_chain_manager/libs/ECCUtils/EthCrossChainUtils.sol
@@ -451,6 +451,7 @@ library ECCUtils {
     
     // won't change memory
     function hexToCompact(bytes memory keyHex) internal pure returns(bytes memory keyCompact) {
+        if (keyHex.length == 0) { return hex"0x00"; }
         uint hexLen = keyHex.length;
         uint compactLen = hexLen/2 + 1;
         uint t = 0;
