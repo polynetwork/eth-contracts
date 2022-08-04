5bb13a295419bde6bff427bf827143de175fc1a0
ChiWanZi1898 <hy.zhu54@gmail.com>
2022-08-04 00:47:24 -0400

    added: hardhat test framework

---
+++ hardhat.config.js
@@ -0,0 +1,7 @@
+require("@nomiclabs/hardhat-ethers");
+require("@nomicfoundation/hardhat-chai-matchers");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.5.17",
+};
