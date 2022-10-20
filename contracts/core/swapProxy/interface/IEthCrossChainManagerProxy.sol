ff7f0182e58be2ce54f7678d29cf4aac23b666ea
Ethan <E@E>
2022-10-20 11:17:03 +0800

    clean up

--- contracts/core/swapProxy/interface/IEthCrossChainManagerProxy.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.5.0;
-
-/**
- * @dev Interface of the EthCrossChainManagerProxy for business contract like LockProxy to obtain the reliable EthCrossChainManager contract hash.
- */
-interface IEthCrossChainManagerProxy {
-    function getEthCrossChainManager() external view returns (address);
-}
--- contracts/core/swapProxy/interface/IFactory.sol
+++
@@ -1,7 +0,0 @@
-  
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.5.0;
-
-interface IFactory {
-    function isPool(address addr) external view returns (bool);
-}
